9867bcdb115ddce35e33a990401721bed530a88c
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-11-25 12:25:18 +0100

    finish migration to spring data

--- module-repository/src/main/java/com/mjc/school/repository/RepoSpringConfig.java
+++ module-repository/src/main/java/com/mjc/school/repository/RepoSpringConfig.java
@@ -1,42 +1,13 @@
 package com.mjc.school.repository;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
 @EnableJpaRepositories
+@EntityScan
 public class RepoSpringConfig {
-    @Bean
-    @Primary
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setJpaDialect(new HibernateJpaDialect());
-        entityManagerFactoryBean.setPackagesToScan("com.mjc.school.repository.model.implementation");
-
-        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
-
-        return entityManagerFactoryBean;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:file:./testdb");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
 }
