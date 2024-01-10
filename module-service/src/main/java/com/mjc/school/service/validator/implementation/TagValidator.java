e513de568c3d06cd17667888a51c068120ca365c
Anatolii Zenin <anatolii.zenin@gmail.com>
2024-01-10 01:54:04 +0100

    improve tag validator

--- module-service/src/main/java/com/mjc/school/service/validator/implementation/TagValidator.java
+++ module-service/src/main/java/com/mjc/school/service/validator/implementation/TagValidator.java
@@ -2,6 +2,7 @@ package com.mjc.school.service.validator.implementation;
 
 import com.mjc.school.service.TagService;
 import com.mjc.school.service.dto.tag.TagDTOReq;
+import com.mjc.school.service.exception.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +16,14 @@ public class TagValidator extends BaseValidator<TagDTOReq> {
     @Override
     public void validate(TagDTOReq req, String reqType) {
         validateString(req.getName(), nameLengthFrom, nameLengthTo);
+        validateTagDoesNotExist(req);
         if (reqType.equals("update")) {
             validateId(req.getId(), tagService);
         }
+    }
+
+    private void validateTagDoesNotExist(TagDTOReq req) {
+        if (tagService.readByName(req.getName()) != null)
+            throw new BadRequestException("tag with provided name already exists: " + req.getName());
     }
 }
